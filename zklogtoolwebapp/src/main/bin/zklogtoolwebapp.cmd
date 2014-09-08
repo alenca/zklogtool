f6d07d97a3c4c497d91c336e7c77ab05d5e3edb7
Alen Caljkusic <scuderiaforever@gmail.com>
2014-09-08 22:03:19 +0200

    Non-ascii characters removed

--- zklogtoolwebapp/src/main/bin/zklogtoolwebapp.cmd
+++ zklogtoolwebapp/src/main/bin/zklogtoolwebapp.cmd
@@ -1,5 +1,5 @@
 # 
-# Copyright 2014 Alen Čaljkušić.
+# Copyright 2014 Alen Caljkusic.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
